00c2156fd3fb366945f2d75e200e85716ca9edbb
978x <136643049+zox46@users.noreply.github.com>
2024-09-14 08:51:25 +0530

    Update x.py

--- x.py
+++ x.py
@@ -1,4 +1,4 @@
-print(' we have deleted it from the github . due to security reason , contact us on the telegram for the script , it's availble . telegram :- @nota_x ')
+print(' we have deleted it from the github . due to security reason , contact us on the telegram for the script , its availble . telegram :- @nota_x ')
 #if you are using this code please give credit . 
 #those who are using it without credit and trying to decrypt this code , are not a son of one father
 #written by @nota_x (telegram)
